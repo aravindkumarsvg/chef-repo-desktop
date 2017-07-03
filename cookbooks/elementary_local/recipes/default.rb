868b3584edad9d286bcbfa7054219a958bcdaccf
ARAVINDKUMARS <aravind.4svg@gmail.com>
2017-07-03 20:42:57 +0530

    1. Modified the docker machine recipe

--- cookbooks/elementary_local/recipes/default.rb
+++ cookbooks/elementary_local/recipes/default.rb
@@ -5,16 +5,16 @@
 # Copyright:: 2017, Aravind Kumar, All Rights Reserved.
 
 #Inculdes Google Chrome recipe
-# include_recipe "elementary_local::google_chrome"
-# # Includes Vim recipe
-# include_recipe "elementary_local::vim"
-# # Includes Git recipe
-# include_recipe "elementary_local::git"
-# # Includes Sublime Text recipe
-# include_recipe "elementary_local::sublime_text"
-# # Includes Virtualbox recipe
-# include_recipe "elementary_local::virtualbox"
-# # Includes Vagrant recipe
-# include_recipe "elementary_local::vagrant"
+include_recipe "elementary_local::google_chrome"
+# Includes Neovim recipe
+include_recipe "elementary_local::neovim"
+# Includes Git recipe
+include_recipe "elementary_local::git"
+# Includes Sublime Text recipe
+include_recipe "elementary_local::sublime_text"
+# Includes Virtualbox recipe
+include_recipe "elementary_local::virtualbox"
+# Includes Vagrant recipe
+include_recipe "elementary_local::vagrant"
 # Includes Docker recipe
 include_recipe "elementary_local::docker"
--- cookbooks/elementary_local/recipes/docker_machine.rb
+++ cookbooks/elementary_local/recipes/docker_machine.rb
@@ -16,3 +16,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Installs Docker Machine
+remote_file node["docker_machine"]["binary"]["path"] do
+  source node["docker_machine"]["binary"]["uri"]
+  mode node["docker_machine"]["binary"]["mode"]
+  action node["docker_machine"]["binary"]["action"].to_sym
+  only_if { defined? node["docker_machine"]["action"] && node["docker_machine"]["action"] == "install" }
+end
+
+# Removes machines created by Docker Machine iff uninstalled
+execute 'remove_machines' do
+  command 'docker-machine rm -f $(docker-machine ls -q)'
+  ignore_failure true
+  only_if 'docker-machine --version'
+  only_if do node["docker_machine"]["action"] == "remove" end
+end
+
+# Removes the Docker Machine binary
+file node["docker_machine"]["binary"]["path"] do
+  action :delete
+  ignore_failure true
+  only_if 'docker-machine --version'
+  only_if do node["docker_machine"]["action"] == "remove" end
+end
