12f94a8f7ca9b017407b6ec6b6436891cd86060d
ARAVINDKUMARS <aravind.4svg@gmail.com>
2017-07-18 22:38:12 +0530

    1. Added chef-repo as current directory in which solo.rb is present

--- solo.rb
+++ solo.rb
@@ -1,4 +1,4 @@
-Chef_Repo_Path = "/home/mutti/workspace/chef/chef-repo-elementary-local"
+Chef_Repo_Path  = ::File.dirname(__FILE__)
 
 file_cache_path ::File.join(Chef_Repo_Path, ".chef")
 cookbook_path ::File.join(Chef_Repo_Path, "cookbooks")
