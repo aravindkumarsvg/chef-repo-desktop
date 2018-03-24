e8d2b287099966d0e04256b4b1fbf70b16ad967e
ARAVINDKUMARS <aravind.4svg@gmail.com>
2018-03-24 08:45:53 +0530

    1. Added network utilities

--- cookbooks/elementary_local/attributes/utilities.rb
+++ cookbooks/elementary_local/attributes/utilities.rb
@@ -1,25 +1,44 @@
 default['utilities']['packages'] = {
+  # Network port scanner
   "nmap" => {
     "action" => "install"
   },
+  # Network usage utilities
+  "bmon" => {
+    "action" => "install"
+  },
+  "tcptrack" => {
+    "action" => "install"
+  },
+  # Overall statistics
   "htop" => {
     "action" => "install"
   },
+  # Contains iostat
   "sysstat" => {
     "action" => "install"
   },
+  # Disk IO usage statistics
+  "atop" => {
+    "action" => "install"
+  },
+  # Downloader
   "axel" => {
     "action" => "install"
   },
+  # Command line automation tool
   "expect" => {
     "action" => "install"
   },
+  # Contains apache benchmark
   "apache2-utils" => {
     "action" => "install"
   },
+  # sticky notes
   "xpad" => {
     "action" => "install"
   },
+  # Android adb, fastboot
   "android-tools-adb" => {
     "action" => "install"
   },
